ec4702c4995c57722d9a0da6f102dbd655e3b99c
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2019-01-25 16:27:39 +0100

    fix exposing server error bug

--- src/entry.js
+++ src/entry.js
@@ -1,18 +1,19 @@
 export class CustomError extends Error {
   constructor(code = 500, customMessage, ...args) {
     super(args);
-
+  
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, CustomError);
     }
 
+    this.message = customMessage;
+    this.code = code;
+    this.date = new Date();
+    // Do not expose the main cause of server to the user for security reasons
     if (code === 500) {
       this.cause = this.message;
       this.message = 'Internal Server Error';
     }
-    this.message = customMessage;
-    this.code = code;
-    this.date = new Date();
   }
 }
 
